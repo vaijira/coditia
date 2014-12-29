8cb675d76e38a8f0a56e8f684b03dc2783ee280a
Jorge Perez Burgos <vaijira@gmail.com>
2014-12-29 20:22:05 +0100

    Using an RSS feed with 10-K files using gaap 2009 norms.

--- src/test/scala/com/coditia/coditia/crawler/SecCrawlerTest.scala
+++ src/test/scala/com/coditia/coditia/crawler/SecCrawlerTest.scala
@@ -1,3 +1,12 @@
+/*
+ * Tests for SEC crawler.
+ *
+ * Copyright (C) 2014 Jorge Perez Burgos <jorge.perez*at*coditia.com>.
+ *
+ * This work is licensed under the terms of the Affero GNU GPL, version 3.
+ * See the LICENSE file in the top-level directory.
+ *
+ */
 package com.coditia.coditia.crawler
 
 import com.coditia.coditia.model.{DBTestKit, TestLiftSession, CoditiaSchema}
@@ -21,20 +30,20 @@ class SecCrawlerTest extends FlatSpec with DBTestKit with TestLiftSession with L
 
   "SEC Crawler crawling specific XBRL RSS for 10-K" should "create a new company" in {
     val crawler = new SecCrawler
-    //original data "http://www.sec.gov/Archives/edgar/monthly/xbrlrss-2006-02.xml"
-    val url = getClass.getResource("/xbrlrss-2006-02.xml")
+    //original data "http://www.sec.gov/Archives/edgar/monthly/xbrlrss-2009-02.xml"
+    val url = getClass.getResource("xbrlrss-2009-02.xml")
     logger.debug("loading file ..." + url.getPath)
     crawler.parseRss(url.getPath, Filing10K)
 
     val companies = from(CoditiaSchema.secCompany)(c =>
-          where(c.cik === 796343) select (c))
+          where(c.cik === 765880) select (c))
 
-    assert(companies.size == 1, "Only one company must have been created")
+    assert(companies.size == 1, "Only one company with cik 765880 been created")
 
     val company = companies.head
 
-    assert(company.cik._1  == 796343, "Company CIK should be 796343 for ADOBE")
-    assert(company.company.name._1 == "ADOBE SYSTEMS INC", "Company name must be ADOBE SYSTEMS INC")
+    assert(company.cik._1  == 765880, "Company CIK should be 765880 for HCP")
+    assert(company.company.name._1 == "HCP, INC.", "Company name must be HCP, INC.")
   }
 
 }
